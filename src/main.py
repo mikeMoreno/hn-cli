d190c4429cb43c145258942b7d723c4849844349
Michael Moreno <michaelmoreno@outlook.com>
2022-07-09 10:22:43 -0400

    linting

--- src/main.py
+++ src/main.py
@@ -1,98 +1,111 @@
-import requests
-import click
 import math
 import webbrowser
+import requests
+import click
 from bs4 import BeautifulSoup
-from SubmissionParser import *
+from submission_parser import SubmissionParser
 
 HN_BASE_URL = "https://news.ycombinator.com/"
-    
+
 def get_article_link(page, rank):
 
     doc = requests.get(f'{HN_BASE_URL}news?p={page}', timeout=2)
 
     soup = BeautifulSoup(doc.text, "html.parser")
-    
+
     submissions = soup.select(".athing")
 
     for submission in submissions:
         submission_info = SubmissionParser.get_submission_info(submission)
-        
+
         if submission_info.rank == rank:
             return submission_info.article_link
     return None
-    
+
 def get_submission_link(page, rank):
 
     doc = requests.get(f'{HN_BASE_URL}news?p={page}', timeout=2)
 
     soup = BeautifulSoup(doc.text, "html.parser")
-    
+
     submissions = soup.select(".athing")
 
     for submission in submissions:
         submission_info = SubmissionParser.get_submission_info(submission)
-        
+
         # TODO: move submission link to submission info object
         if submission_info.rank == rank:
             return f"{HN_BASE_URL}item?id={submission_info.id}"
     return None
-    
-def format(submission_info, hide_ranks):
-        
+
+def display_submission(submission_info, hide_ranks):
+
     if hide_ranks:
         return f"{submission_info.title}"
-    else:
-        rank = f"[{submission_info.rank}]"
-        
-        if submission_info.rank < 10:
-            rank = f" {rank}"
-        return f"{rank} {submission_info.title}"
 
-def display_submissions(page = 1, hide_ranks = False):
+    rank = f"[{submission_info.rank}]"
+
+    if submission_info.rank < 10:
+        rank = f" {rank}"
+    return f"{rank} {submission_info.title}"
+
+def get_submissions(page = 1):
     doc = requests.get(f'{HN_BASE_URL}news?p={page}', timeout=2)
 
     soup = BeautifulSoup(doc.text, "html.parser")
-    
-    submissions = soup.select(".athing")
-    
+
+    submission_elements = soup.select(".athing")
+
+    submissions = []
+
+    for submission_element in submission_elements:
+        submission_info = SubmissionParser.get_submission_info(submission_element)
+        submissions.append(submission_info)
+
+    return submissions
+        #print(format_(submission_info, hide_ranks))
+
+def display_submissions(page = 1, hide_ranks = False):
+
+    submissions = get_submissions(page)
+
     for submission in submissions:
-        submission_info = SubmissionParser.get_submission_info(submission)
-        
-        print(format(submission_info, hide_ranks))
-        
+        #submission_info = SubmissionParser.get_submission_info(submission)
+
+        print(display_submission(submission, hide_ranks))
+
 @click.command()
 @click.option('--page', "-p", type=int, help="Specify the page to display")
 @click.option('--article', "-a", type=int, help="Specify the article to open")
 @click.option('--submission', "-s", type=int, help="Specify the submission to open")
 @click.option('--hide-ranks', is_flag=True, default=False, help="Hide ranks on output")
 def main(page, submission, article, hide_ranks):
-    SUBMISSIONS_PER_PAGE = 30
-    
+    submissions_per_page = 30
+
     if page is not None:
         display_submissions(page, hide_ranks)
-        
+
         return
-        
+
     if article is not None:
-        page = math.ceil(article / (SUBMISSIONS_PER_PAGE * 1.0))
-        
+        page = math.ceil(article / (submissions_per_page * 1.0))
+
         link = get_article_link(page, article)
-        
+
         webbrowser.open(link)
-        
+
         return
-    
+
     if submission is not None:
-        page = math.ceil(submission / (SUBMISSIONS_PER_PAGE * 1.0))
-        
+        page = math.ceil(submission / (submissions_per_page * 1.0))
+
         link = get_submission_link(page, submission)
 
         webbrowser.open(link)
 
-        return        
+        return
 
     display_submissions(hide_ranks = hide_ranks)
-    
+
 if __name__ == '__main__':
-    main()
+    main() # pylint: disable=no-value-for-parameter
--- src/submission_parser.py
+++ src/submission_parser.py
@@ -1,69 +1,69 @@
-from SubmissionInfo import *
+from submission_info import SubmissionInfo
 
 class SubmissionParser:
 
     @classmethod
     def _get_title_text(cls, title_info):
         title_text = title_info.text
-        
+
         last_paren_index = title_text.rfind("(")
-        
+
         if last_paren_index < 0:
             return title_text
-        
+
         return title_text[:last_paren_index]
 
     @classmethod
     def _get_title_info(cls, submission):
         title_index = 0
-        
+
         for child in submission.find_all(recursive=False):
-             if "class" in child.attrs and child.attrs["class"][0] == "title":
+            if "class" in child.attrs and child.attrs["class"][0] == "title":
                 title_index+= 1
-                
-             if title_index == 2:
+
+            if title_index == 2:
                 return child
         return None
-        
+
     @classmethod
     def _parse_article_link(cls, title_info):
 
         article_link = title_info.find_all("a")[0].attrs["href"]
-        
+
         if not article_link.startswith("http"):
             article_link = f"https://news.ycombinator.com/{article_link}"
-        
+
         return article_link
-    
+
     @classmethod
     def _get_rank(cls, submission):
         title_index = 0
-        
+
         for child in submission.find_all(recursive=False):
-             if "class" in child.attrs and child.attrs["class"][0] == "title":
+            if "class" in child.attrs and child.attrs["class"][0] == "title":
                 title_index+= 1
-                
-             if title_index == 1:
+
+            if title_index == 1:
                 rank = child.text
-                
+
                 if rank is None:
                     return None
                 return int(rank.replace(".", "").strip())
-                
+
         return None
 
     @classmethod
     def get_submission_info(cls, submission):
-        
+
         submission_info = SubmissionInfo()
-        
+
         submission_info.id = submission.attrs["id"]
-        
+
         submission_info.rank = cls._get_rank(submission)
-        
+
         title_info = cls._get_title_info(submission)
-        
+
         submission_info.title = cls._get_title_text(title_info)
         submission_info.article_link = cls._parse_article_link(title_info)
-            
+
         return submission_info
