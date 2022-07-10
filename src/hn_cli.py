3ecb618838a36aa893fdf4f0b5e17b9b34d05e4e
Michael Moreno <michaelmoreno@outlook.com>
2022-07-10 14:29:44 -0400

    format points, remove hide-ranks feature

--- src/hn_cli.py
+++ src/hn_cli.py
@@ -3,16 +3,29 @@ import click
 from hn_client import HNClient
 from submission_parser import SubmissionParser
 
-def format_submission(submission_info, hide_ranks):
+def format_rank(submission_rank):
+    rank = f"[{submission_rank}]"
 
-    if hide_ranks:
-        return f"{submission_info.title}"
-
-    rank = f"[{submission_info.rank}]"
-
-    if submission_info.rank < 10:
+    if submission_rank < 10:
         rank = f" {rank}"
-    return f"{rank} |{submission_info.points}| {submission_info.title}"
+
+    return rank
+
+def format_points(submission_points, highest_point_digits):
+    points = f"{submission_points}|"
+
+    while len(str(points)) <= highest_point_digits:
+        points = " " + points
+    points = "|" + points
+
+    return points
+
+def format_submission(submission_info, highest_point_digits):
+
+    rank = format_rank(submission_info.rank)
+    points = format_points(submission_info.points, highest_point_digits)
+
+    return f"{rank} {points} {submission_info.title}"
 
 def display_karma(hn_client, profile):
     karma = hn_client.get_karma(profile)
@@ -28,20 +41,21 @@ def display_submission(hn_client, rank):
     submission_info = hn_client.get_submission(rank)
     webbrowser.open(submission_info.submission_link)
 
-def display_submissions(hn_client, page, hide_ranks):
+def display_submissions(hn_client, page):
 
     submissions = hn_client.get_submissions(page)
 
+    highest_point_digits = len(str(max([s.points for s in submissions])))
+
     for submission in submissions:
-        print(format_submission(submission, hide_ranks))
+        print(format_submission(submission, highest_point_digits))
 
 @click.command()
 @click.option('--page', "-p", type=int, default=1, show_default=True, help="Display the specified page")
 @click.option('--article', "-a", type=int, help="Open the specified article")
 @click.option('--submission', "-s", type=int, help="Open the specified submission")
 @click.option('--karma', "-k", "profile", help="Display the karma of the specified profile")
-@click.option('--hide-ranks', is_flag=True, default=False, help="Hide ranks on output")
-def main(page, submission, article, hide_ranks, profile):
+def main(page, submission, article, profile):
 
     HN_BASE_URL = "https://news.ycombinator.com/" # pylint: disable=invalid-name
 
@@ -62,7 +76,7 @@ def main(page, submission, article, hide_ranks, profile):
 
         return
 
-    display_submissions(hn_client, page, hide_ranks)
+    display_submissions(hn_client, page)
 
 if __name__ == '__main__':
     main() # pylint: disable=no-value-for-parameter
